86fa414b8ab0ac1a768c7edba6e05b9ae72abc65
fbwkzl333 <fbwkzl111@naver.com>
2016-10-21 22:56:32 +0900

    2. 직각삼각 별찍기 완료 (주석완료)

--- homework5/src/homework5/RectagleStars.java
+++ homework5/src/homework5/RectagleStars.java
@@ -8,7 +8,7 @@ public class RectagleStars {
 		Scanner s = new Scanner(System.in);
 		System.out.print("정사각형 크기입력 : ");
 		int num = s.nextInt();  // 정사각형의 가로,세로 길이 값을 입력받음
-		for(int i = 0; i <num; i++){  // 세로길이 입력된 num 만큼 * 출력
+		for(int i = 0; i <(num/2); i++){  // 세로길이 입력된 num/2(개행 여백: 1줄) 만큼 * 출력
 			for(int j=0; j<num; j++){  // 가로길이 입력된 num 만큼 * 출력
 				System.out.print("*");
 			}System.out.println();  // 가로 한줄 완성마다 개행

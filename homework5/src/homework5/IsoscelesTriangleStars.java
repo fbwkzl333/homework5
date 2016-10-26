f39de6c866f993f32beff7df0be8d7f6a9ff1f14
fbwkzl333 <fbwkzl111@naver.com>
2016-10-27 03:41:14 +0900

    3. 이등변삼각형 별찍기 완료 (주석완료)

--- homework5/src/homework5/IsoscelesTriangleStars.java
+++ homework5/src/homework5/IsoscelesTriangleStars.java
@@ -9,14 +9,15 @@ public class IsoscelesTriangleStars {
 		System.out.print("이등변삼각형 높이입력 : ");
 		int input = s.nextInt();  // 높이
 		int star =0;  // * 그리기 할 녀석
-		int gap = input;  // 공백을 그릴 녀석, 입력받은 높이값을 넣어줌 (높이값을 시작으로 점점 작아지게 써먹을 예정)
+		int gap = input;  // 공백을 그릴 녀석, 입력받은 크기값을 넣어줌 (입력값을 기준으로 공백의 갯수를 정하는데 쓰임)
 		
 		for(int i=0; i<input; i++){  // input만큼 줄 입력(높이)  [ 높이 for문 시작]
 			
-			for(int j=0; j<=gap; j++){System.out.print(" ");}// gap 만큼 공백을 만듬("공백길이")
-			for(int k=0; k <=star; k++){System.out.print("*");}// star만큼 *을 출력함 (*갯수)
+			for(int j=0; j<=gap; j++){System.out.print(" ");} // gap 만큼 공백을 만듬("공백길이")
+			for(int k=0; k <=star; k++){System.out.print("*");} // star만큼 *을 출력함 (*갯수)
 				
 			System.out.println();  // 한줄 개행(한줄이 끝남, 이제 공백, *갯수 변동주고 다시 똑같이 실행)		[ 이곳은 높이 for문]
+			
 			gap--;  // h를 1줄임 : " "를 전보다 1개씩 덜 출력하겠다.  (이등변삼각형 모양을 위해 한칸 왼쪽까지만 공백 출력)
 			star+=2;  // star를 2개 늘림 : *를 전보다 2개씩 더 출력하겠다. ( nul 자리가 왼쪽으로 옮겨지니 *을 2개씩 더 출력시 이등변삼각형 완성)
 		}
